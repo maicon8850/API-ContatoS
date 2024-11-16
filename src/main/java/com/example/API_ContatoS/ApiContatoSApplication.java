e9ceca245e42dd67adb57b25f89cabba4d0a9c7a
maicon-douglas <maicon.douglas3527@gmail.com>
2024-11-16 13:44:09 -0300

    Initial commit

---
+++ src/main/java/com/example/API_ContatoS/ApiContatoSApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_ContatoS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContatoSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContatoSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_ContatoS/ApiContatoSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_ContatoS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContatoSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
