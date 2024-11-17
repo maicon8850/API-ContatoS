d8360b8cd0bda645c7aa1e34c359a48020815e8f
maicon-douglas <maicon.douglas3527@gmail.com>
2024-11-16 23:48:24 -0300

    feat: Implementação de CRUD para gerenciamento de contatos
    - Adiciona endpoint GET para listar todos os contatos e buscar por ID.
    - Implementa endpoint POST para criação de novos contatos.
    - Adiciona suporte para atualização de contatos com o método PUT.
    - Implementa a remoção de contatos pelo ID com o método DELETE.

--- src/main/java/Controlador/Controller.java
+++ src/main/java/Controlador/Controller.java
@@ -2,9 +2,12 @@ package Controlador;
 //O Controller é a classe responsável por expor cada URI que estará disponível na API.
 // O código inicial está listado abaixo:
 
+import Entidade.Contact;
 import Repositorio.ContactRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping({"/contacts"}) // indicando q
@@ -15,5 +18,51 @@ public class Controller {
     Controller (ContactRepository contactRepository ){
         this.repository = contactRepository;
     }
-    // metodo do CRUD aqu i
+    // metodo do CRUD
+    //Listando todos os contatos (GET /contacts)
+    @GetMapping
+    public List findALL(){
+        return repository.findAll(); // metodo para retornar uma lissta de entidades
+    }
+    //Obtendo um contato especídifo pelo ID (GET /contacts/{id})
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable long id){
+        return repository.findById(id)  //findById é da interfase JPA
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    //Criando um novo contato (POST /contacts)
+    // código para criar um novo contato está listado
+    @PostMapping
+    public Contact create(@RequestBody Contact contact){
+        return repository.save(contact);
+
+        //{
+        //   "name": "Java",
+        //   "email": "java@email.com",
+        //   "phone": "(111) 111-1111"
+        //}
+    }
+    //Atualizando um contato (PUT /contacts)
+    @PutMapping(value="/{id}")
+    public ResponseEntity update(@PathVariable("id") long id,
+                                 @RequestBody Contact contact) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    //Removendo um contato pelo ID (DELETE /contacts/{id})
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity <?> delete(@PathVariable long id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
